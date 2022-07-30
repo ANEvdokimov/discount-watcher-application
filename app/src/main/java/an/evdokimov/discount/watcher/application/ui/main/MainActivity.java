3c3f34ed76b45873f3789421024683f6016121c9
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2022-07-30 14:03:52 +0600

    add mode list onto main screen

--- app/src/main/java/an/evdokimov/discount/watcher/application/ui/main/MainActivity.java
+++ app/src/main/java/an/evdokimov/discount/watcher/application/ui/main/MainActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 
+import an.evdokimov.discount.watcher.application.R;
 import an.evdokimov.discount.watcher.application.configuration.DiscountWatcherApplication;
 import an.evdokimov.discount.watcher.application.configuration.ExceptionService;
 import an.evdokimov.discount.watcher.application.data.database.user.model.User;
@@ -45,25 +49,9 @@ public class MainActivity extends AppCompatActivity {
         ((DiscountWatcherApplication) getApplicationContext())
                 .applicationComponent.inject(this);
 
-        shopSpinner = binding.shopSpinner;
-        shopSpinner.setAdapter(shopSpinnerAdapter);
-        shopSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                productListAdapter.updateProducts();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                shopSpinner.setSelection(0);
-            }
-        });
-
-        modeSpinner = binding.modeSpinner;
-
-        RecyclerView productList = binding.productList;
-        productList.setLayoutManager(new LinearLayoutManager(this));
-        productList.setAdapter(productListAdapter);
+        configureModeSpinner();
+        configureShopSpinner();
+        configureProductList();
 
         userService.getActive()
                 .subscribeOn(Schedulers.io())
@@ -94,5 +82,50 @@ public class MainActivity extends AppCompatActivity {
 
     protected void updateContent() {
         shopSpinnerAdapter.updateShopSpinner();
+    }
+
+    private void configureShopSpinner() {
+        shopSpinner = binding.shopSpinner;
+        shopSpinner.setAdapter(shopSpinnerAdapter);
+        shopSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                productListAdapter.updateProducts();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                shopSpinner.setSelection(0);
+            }
+        });
+    }
+
+    private void configureModeSpinner() {
+        modeSpinner = binding.modeSpinner;
+        ArrayList<String> modeList = new ArrayList<>();
+        modeList.add(getResources().getString(R.string.main_mode_spinner_all));
+        modeList.add(getResources().getString(R.string.main_mode_spinner_discount));
+        modeList.add(getResources().getString(R.string.main_mode_spinner_available));
+        modeList.add(getResources().getString(R.string.main_mode_spinner_price_changed));
+        modeSpinner.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, modeList)
+        );
+        modeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                productListAdapter.updateProducts();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                modeSpinner.setSelection(0);
+            }
+        });
+    }
+
+    private void configureProductList() {
+        RecyclerView productList = binding.productList;
+        productList.setLayoutManager(new LinearLayoutManager(this));
+        productList.setAdapter(productListAdapter);
     }
 }
